4031a966ffe6e57d8f15f326afe2ac377577e082
Dejmenek <dejmen14567@gmail.com>
2024-11-22 22:40:13 +0100

    Implement inventory

---
+++ AdvancedClasses/Inventory.cs
@@ -0,0 +1,13 @@
+﻿namespace AdvancedClasses;
+public class Inventory
+{
+    private readonly List<IItem> _items = new List<IItem>();
+
+    public void Add(IItem item) => _items.Add(item);
+
+    public void Remove(IItem item) => _items.Remove(item);
+
+    public void DisplayItems() => _items.ForEach(i => Console.WriteLine(i.GetDescription()));
+
+    public void UseItems() => _items.ForEach(i => i.Use());
+}
